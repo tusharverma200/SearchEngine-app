807ffe75bc1b3977978a3bc9472a5b1e6f00286d
Tushar Verma <vermatushar200@gmail.com>
2025-04-10 00:12:26 +0530

    first commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-const BASE_URL="https://www.googleapis.com/customsearch/v1"
-const params ={
-    key:'AIzaSyCjGDRJMvcJzSRa5Sl70v_ZelKZ1mtEP2c',
-    cx:'87666ff7e9d9c4a5a'
+const BASE_URL = "https://www.googleapis.com/customsearch/v1"
+const params = {
+    key: process.env.GOOGLE_SEARCH_API_KEY,
+    cx: process.env.GOOGLE_SEARCH_CX,
 }
 export const fetchDataFromApi = async (payload) => {
-    const {data}= await axios.get(BASE_URL, {
-        params: {...params, ...payload}
+    const { data } = await axios.get(BASE_URL, {
+        params: { ...params, ...payload }
     })
     return data;
 };
